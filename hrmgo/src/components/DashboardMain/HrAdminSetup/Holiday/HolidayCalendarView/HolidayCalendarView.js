8e9c42262d6f1dd1d4333043acaf1fa8d2630c9c
nikitabaladha <nikitabaladha1996@gmail.com>
2024-12-04 09:22:51 +0530

    Holiday Calendar page serch form for holiday list done minor setup remaining for calendar according to search remaining

--- hrmgo/src/components/DashboardMain/HrAdminSetup/Holiday/HolidayCalendarView/HolidayCalendarView.js
+++ hrmgo/src/components/DashboardMain/HrAdminSetup/Holiday/HolidayCalendarView/HolidayCalendarView.js
@@ -1,19 +1,74 @@
+// import React, { useState, useEffect } from "react";
+// import HolidayCalendarHeader from "./HolidayCalendarHeader";
+// import HolidayCalendarSearchForm from "./HolidayCalendarSearchForm";
+
+// import HolidayCalendar from "./HolidayCalendar";
+// import HolidayList from "./HolidayList";
+
+// const HolidayCalendarView = ({
+//   holidays,
+//   selectedHoliday,
+//   setSelectedHoliday,
+// }) => {
+//   return (
+//     <>
+//       <HolidayCalendarHeader />
+//       <HolidayCalendarSearchForm />
+//       <div className="row">
+//         <HolidayCalendar
+//           holidays={holidays}
+//           selectedHoliday={selectedHoliday}
+//           setSelectedHoliday={setSelectedHoliday}
+//         />
+//         <HolidayList holidays={holidays} />
+//       </div>
+//     </>
+//   );
+// };
+
+// export default HolidayCalendarView;
+
 import React, { useState, useEffect } from "react";
 import HolidayCalendarHeader from "./HolidayCalendarHeader";
 import HolidayCalendarSearchForm from "./HolidayCalendarSearchForm";
 
+import getAPI from "../../../../../api/getAPI";
+
 import HolidayCalendar from "./HolidayCalendar";
 import HolidayList from "./HolidayList";
 
-const HolidayCalendarView = ({
-  holidays,
-  selectedHoliday,
-  setSelectedHoliday,
-}) => {
+const HolidayCalendarView = () => {
+  const [holidays, setHolidays] = useState([]);
+  const [selectedHoliday, setSelectedHoliday] = useState(null);
+
+  useEffect(() => {
+    const fetchHolidayData = async () => {
+      try {
+        const response = await getAPI(`/holiday`, {}, true);
+        if (
+          !response.hasError &&
+          response.data &&
+          Array.isArray(response.data.data)
+        ) {
+          setHolidays(response.data.data);
+        } else {
+          console.error("Invalid response format or error in response");
+        }
+      } catch (err) {
+        console.error("Error fetching Holiday Data:", err);
+      }
+    };
+
+    fetchHolidayData();
+  }, []);
+
+  const handleSearchResults = (searchedHolidays) => {
+    setHolidays(searchedHolidays);
+  };
   return (
     <>
       <HolidayCalendarHeader />
-      <HolidayCalendarSearchForm />
+      <HolidayCalendarSearchForm onSearchResults={handleSearchResults} />
       <div className="row">
         <HolidayCalendar
           holidays={holidays}
